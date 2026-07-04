fef114a559e60cfb1edaf6e1f8f44d3be94ab03d
fsh <640372469@qq.com>
2026-07-04 17:49:11 +0800

    慢病管理平台基础

---
+++ chronic-disease-common/src/main/java/com/hanghang/constant/MqConstant.java
@@ -0,0 +1,13 @@
+package com.hanghang.constant;
+
+public interface MqConstant {
+
+    String REMIND_TOPIC = "remind-topic";
+
+    String DOCTOR_MESSAGE_TOPIC = "doctor-message-topic";
+
+    String INDEX_ARCHIVE_TOPIC = "index-archive-topic";
+
+    String OPER_LOG_TOPIC = "oper-log-topic";
+
+}
---
+++ chronic-disease-common/src/main/java/com/hanghang/constant/RedisConstant.java
@@ -0,0 +1,15 @@
+package com.hanghang.constant;
+
+public interface RedisConstant {
+
+    String USER_LOGIN_KEY = "user:login:";
+
+    String INDEX_DICT_KEY = "index:dict";
+
+    String HOT_ARTICLE_KEY = "article:hot";
+
+    long DEFAULT_EXPIRE_TIME = 3600L;
+
+    long USER_TOKEN_EXPIRE_TIME = 7200L;
+
+}
